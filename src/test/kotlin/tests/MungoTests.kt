580d3282a7f7d3b7c288a0d70ea8626eb52efa13
João Mota <jdmota@outlook.pt>
2020-05-19 17:57:04 +0100

    Refactor test bootstrapping

---
+++ src/test/kotlin/tests/MungoTests.kt
@@ -0,0 +1,57 @@
+package tests
+
+import org.junit.runners.Parameterized.Parameters
+import java.io.File
+
+private const val dir = "config"
+
+class MungoConfigTest(testFiles: List<File>) : MungoPerDirectoryTest(
+  dir,
+  testFiles,
+  arrayOf("-Astubs=tests/$dir/stubs", "-AstubWarnIfNotFound", "-Aignorejdkastub")
+) {
+  companion object {
+    @JvmStatic
+    @get:Parameters
+    val testDirs = arrayOf(dir)
+  }
+}
+
+private const val dir2 = "droppables"
+
+class MungoDroppablesTest(testFiles: List<File>) : MungoPerDirectoryTest(
+  dir2,
+  testFiles
+) {
+  companion object {
+    @JvmStatic
+    @get:Parameters
+    val testDirs = arrayOf(dir2)
+  }
+}
+
+private const val dir3 = "resolution"
+
+class MungoResolutionTest(testFiles: List<File>) : MungoPerDirectoryTest(
+  dir3,
+  testFiles
+) {
+  companion object {
+    @JvmStatic
+    @get:Parameters
+    val testDirs = arrayOf(dir3)
+  }
+}
+
+private const val dir4 = "mungo"
+
+class MungoTest(testFiles: List<File>) : MungoPerDirectoryTest(
+  dir4,
+  testFiles
+) {
+  companion object {
+    @JvmStatic
+    @get:Parameters
+    val testDirs = arrayOf(dir4)
+  }
+}
