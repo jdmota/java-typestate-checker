8ad2a66f2605da2c1fb79fb0ba9857e8a3178312
João Mota <jdmota@outlook.pt>
2021-01-31 16:26:16 +0000

    LineReader example: uncomment example with data race

--- examples/line-reader-example/Main.java
+++ examples/line-reader-example/Main.java
@@ -95,7 +95,7 @@ public class Main {
   }*/
 
   // java.io.IOException: Stream closed
-  /*public static void dataRace() throws Exception {
+  public static void dataRace() throws Exception {
     LineReader reader = new LineReader();
 
     if (reader.open("LineReader.java") == Status.OK) {
@@ -110,30 +110,30 @@ public class Main {
       reader.close();
 
       thread.join();
-    } else {
-      System.err.println("Could not open file");
-    }
-  }*/
-
-  public static void noDataRace() throws Exception {
-    LineReader reader = new LineReader();
-
-    if (reader.open("LineReader.java") == Status.OK) {
-      Thread thread = new Thread(() -> {try{
-        while (!reader.eof()) {
-          System.out.println(reader.read());
-        }
-      }catch(Exception exp){System.err.println("In thread:");exp.printStackTrace();}});
-
-      thread.start();
-
-      thread.join();
-
-      reader.close();
     } else {
       System.err.println("Could not open file");
     }
   }
+
+  /*public static void noDataRace() throws Exception {
+    LineReader reader = new LineReader();
+
+    if (reader.open("LineReader.java") == Status.OK) {
+      Thread thread = new Thread(() -> {try{
+        while (!reader.eof()) {
+          System.out.println(reader.read());
+        }
+      }catch(Exception exp){System.err.println("In thread:");exp.printStackTrace();}});
+
+      thread.start();
+
+      thread.join();
+
+      reader.close();
+    } else {
+      System.err.println("Could not open file");
+    }
+  }*/
 
   // Hidden error
   /*public static void dataRace2() throws Exception {
