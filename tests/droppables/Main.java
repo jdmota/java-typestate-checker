adaa685d6888a8be1464762b43f41dc8695be2bf
João Mota <jdmota@outlook.pt>
2022-12-07 18:24:20 +0000

    Add test for warning that object will be dropped

--- tests/droppables/Main.java
+++ tests/droppables/Main.java
@@ -1,8 +1,15 @@
 public class Main {
-	public static void main(String[] args) {
-		MyComparator comparator = new MyComparator();
-		// :: warning: (comparator: State{MyComparator, Start})
+  public static void main(String[] args) {
+    MyComparator comparator = new MyComparator();
+    // :: warning: (comparator: State{MyComparator, Start})
     // :: warning: (java.lang.System.out: Shared{java.io.PrintStream})
     System.out.println(comparator.compare(10, 5));
-	}
+    // :: warning: (comparator: State{MyComparator, Start})
+    // :: warning: (Object [comparator] with type State{MyComparator, Start} will be dropped)
+    use(comparator);
+  }
+
+  public static void use(MyComparator comparator) {
+
+  }
 }
