a8fa526733a24d2af13cd333d7caab7e2f73a0ec
LBacchiani <>
2023-04-21 12:30:29 +0200

    fix error

---
+++ tests/basic/MissingMethod.java
@@ -0,0 +1,19 @@
+import jatyc.lib.Typestate;
+
+@Typestate("JavaIterator.protocol")
+public class MissingMethod {
+
+  public boolean hasNext() {
+    return false;
+  }
+
+  public String next() {
+    return "";
+  }
+
+  // (Method [unusedMethod] does not appear in the typestate)
+  public void unusedMethod() {
+
+  }
+
+}
