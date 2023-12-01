4e7498fdb13c57ad6cab12f0cf9a20d898b66217
João Mota <jdmota@outlook.pt>
2023-12-01 12:43:41 +0100

    Update car1 example

--- examples/car-example/ClientCode.java
+++ examples/car-example/ClientCode.java
@@ -1,18 +1,19 @@
-import jatyc.lib.Requires;
-
 public class ClientCode {
-  public static void main(String[] args) {
-    SUV sc = new SUV();
-    while (!sc.turnOn()) { System.out.println("turning on..."); }
-    sc.switchMode();
-    switchSUVAndSetSpeed(sc);
-    System.out.println("Done!");
+  public static void example() {
+    SUV suv = new SUV();
+    while (!suv.turnOn()) { System.out.println("turning on..."); }
+    suv.switchMode();
+    setSpeed(suv);
   }
-  private static void switchSUVAndSetSpeed(@Requires("ON") Car c) {
-    if (c instanceof SUV && ((SUV) c).switchMode() == Mode.SPORT){
-      ((SUV) c).setFourWheels(true);
-    }
-    c.setSpeed(50);
-    c.turnOff();
+  private static void setSpeed(@jatyc.lib.Requires("ON") Car car) {
+    if (car instanceof SUV && ((SUV) car).switchMode() == Mode.SPORT)
+      ((SUV) car).setFourWheels(true);
+    car.setSpeed(50);
+    car.turnOff();
+  }
+
+  public static void main(String[] args) {
+    example();
+    System.out.println("Done!");
   }
 }
