12211936584d4c9c24c24bbe4fd618deea14fe5b
LBacchiani <lorenzo.bacchiani2@unibo.it>
2023-12-01 11:43:55 +0100

    minor car3

--- examples/car-example3/Car.java
+++ examples/car-example3/Car.java
@@ -2,7 +2,7 @@ import jatyc.lib.Typestate;
 
 @Typestate("Car")
 public class Car {
-  public boolean turnOn() { return false; }
+  public boolean turnOn() { return true; }
   public void setSpeed(int b) {}
   public void turnOff() {}
 }
