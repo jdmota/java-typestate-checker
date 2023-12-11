1b93627ef4617ff6bfaa4824892f632e9ff09a73
LBacchiani <lorenzo.bacchiani2@unibo.it>
2023-12-11 17:33:49 +0100

    setSpeed overriding in car example

--- examples/car-example/SUV.java
+++ examples/car-example/SUV.java
@@ -2,7 +2,40 @@ import jatyc.lib.Typestate;
 
 @Typestate("SUV")
 public class SUV extends Car {
-  public Mode switchMode() { return Mode.SPORT; }
-  public void setEcoDrive(boolean state) {}
-  public void setFourWheels(boolean state) {}
+
+  private final int THRESHOLD = 80;
+  private Mode mode;
+  private boolean ecoDrive;
+  private boolean fourWheels;
+  private int speed;
+
+  public SUV() {
+    this.mode = Mode.COMFORT;
+    this.ecoDrive = false;
+    this.fourWheels = false;
+    this.speed = 0;
+  }
+  public Mode switchMode() {
+    if(mode == Mode.SPORT) {
+      mode = Mode.COMFORT;
+      fourWheels = false;
+    }
+    else {
+      mode = Mode.SPORT;
+      ecoDrive = false;
+    }
+    return mode;
+  }
+  public void setEcoDrive(boolean state) {
+    ecoDrive = state;
+  }
+  public void setFourWheels(boolean state) {
+    fourWheels = state;
+  }
+
+//override
+  public void setSpeed(int b) {
+    if(ecoDrive) speed = b < THRESHOLD ? b : THRESHOLD;
+    else speed = b;
+  }
 }
