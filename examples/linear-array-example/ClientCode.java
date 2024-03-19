b739bdbfc7713520d56fa1834b41ee06b477a25d
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-03-19 13:02:18 +0000

    simplify

--- examples/linear-array-example/ClientCode.java
+++ examples/linear-array-example/ClientCode.java
@@ -3,9 +3,11 @@ public class ClientCode {
   public static void main(String[] args) {
     int n = 5;
     Car[] cars = new Car[n];
-    Car[] cars2 = new Car[]{new Car(), new SUV(), new Car()};
+    Car c = new Car();
+    Car[] cars2 = new Car[]{c, new SUV(), new Car()};
     Car[][] cars3 = new Car[n][n];
     Car[] cars4 = new Car[5];
     Car[][] cars5 = new Car[5][5];
+    c.turnOn();
   }
 }
