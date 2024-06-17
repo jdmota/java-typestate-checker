003e289e425ebe9c27f879676a74a2c59403fe03
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-06-17 16:20:58 +0200

    fix getTypeFromString + array example

--- examples/array-example/ClientCode.java
+++ examples/array-example/ClientCode.java
@@ -6,24 +6,9 @@ public class ClientCode {
     int x = 5;
     Car[] cars = new Car[x];
     for (int i = 0; i < 5; i++) {
-      // cars[i] = new Car();
-      System.out.println(i);
-    }
-    Car[] cars2 = cars;
-  }
-
-  public static void main2() {
-    int i = 2;
-    boolean cond = true;
-    if (cond) i = 5;
-    Car[] cars = new Car[7];
-    Car c = new Car();
-    cars[i] = c;
-    c = cars[i];
-
-    while (true) {
-      Car[] x = new Car[7];
-      assert Utils.loopInvariant(x, "Off", i, "null") : "";
+      cars[i] = new Car();
+      assert Utils.loopInvariant(cars, "null", i, "OFF") : "";
     }
   }
 }
+
