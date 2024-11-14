81aaff3db42f0ac553c6467c5034d8e5367f75e9
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-11-14 11:11:27 +0100

    chore(examples)

--- examples/theory-adherence-examples/store-boolean-in-var/Main.java
+++ examples/theory-adherence-examples/store-boolean-in-var/Main.java
@@ -1,17 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     A a = new A(); //tool: A1, theory: A1
-    boolean x = a.m1(); //tool: A1 U A2, theory: <A2,A1>
-    if (!x) {
-      //tool: A1 U A2, theory: <A1>
-      while (!a.m1()) { /*tool: A1 U A2 (error), theory: A1*/}
-    } else {
-      a.m2(); //tool: A1 U A2 (error), theory: end
-    }
-  }
-
-  public static void main2(String[] args) {
-    A a = new A(); //tool: A1, theory: A1
     boolean x = false;
     if (x = a.m1()) {
       //tool: A1 U A2, theory: A2
