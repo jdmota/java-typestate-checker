99b80eefca18276d0253f14bc4450baa95f2701b
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-11-15 10:53:51 +0100

    chore(wrapper example)

--- examples/theory-adherence-examples/object-wrapper/ClientCode.java
+++ examples/theory-adherence-examples/object-wrapper/ClientCode.java
@@ -1,7 +1,6 @@
 public class ClientCode {
   public static void main(String[] args) {
     Wrapper w = new Wrapper();
-    w.init(new JavaIterator());
     while (w.hasNext()) {
       w.next();
     }
--- examples/theory-adherence-examples/object-wrapper/Wrapper.java
+++ examples/theory-adherence-examples/object-wrapper/Wrapper.java
@@ -6,25 +6,20 @@ import jatyc.lib.*;
 
 import java.util.function.Supplier;
 
-@Typestate("Wrapper.protocol")
+@Typestate("Iterator.protocol")
 class Wrapper {
 
   private @Nullable JavaIterator iterator = null;
 
-  public void init(@Requires("HasNext") JavaIterator it) {
-    // :: warning: (this.iterator: Null)
-    // :: warning: (it: State{JavaIterator, HasNext})
-    iterator = it;
+  public Wrapper() {
+    iterator = new JavaIterator();
   }
 
   public boolean hasNext() {
-    // :: warning: (this.iterator: State{JavaIterator, HasNext})
-    // :: warning: (this.iterator: State{JavaIterator, Next})
     return iterator.hasNext();
   }
 
   public String next() {
-    // :: warning: (this.iterator: State{JavaIterator, Next})
     return iterator.next();
   }
 }
